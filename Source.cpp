92f1bfb449f8d1a7a4f85a6185a4b0afe355dcdb
Tempest <hoangcong25022003@gmail.com>
2024-11-02 09:03:14 +0700

    tinh tich thuong

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,23 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
+int tich(int a, int b){
+	return a*b;
+}
+float thuong(int a, int b){
+	return float(a)/b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a;
+	int b;
+	cout<<"Nhap a: ";
+	cin>>a;
+	cout<<"Nap b: ";
+	cin>>b;
+	cout<<"Tich la: "<<tich(a,b)<<endl;
+	cout<<"Thuong la: "<<thuong(a,b)<<endl;
 	system("pause");
 	return 0;
 }
