2854ea6aa3b5893bf3d8b7ffab84f83d9476aa52
duybanit <nguyenduyban02@gmail.com>
2024-11-02 09:02:58 +0700

    tinh tru

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	int a,b;
+	cout<<"Nhap a:";
+	cin>>a;
+	cout<<"Nhap b:";
+	cin>>b;
+	cout<<"hieu la:<<hieu(a,b)<<endl;
 	system("pause");
 	return 0;
 }
